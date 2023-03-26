931ddedf50e946ef3fba982241d1490b994898cf
Randy Duodu <duodurandy19@gmail.com>
2023-03-26 04:31:57 +0000

    Added the popover role and made some minor changes

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -3,3 +3,18 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
   trigger: 'focus'
 })
 
+function makeTablesResponsive() {
+  // Get all table elements on the page
+  const tables = document.getElementsByTagName('table');
+
+  // Wrap each table in a div with the class 'responsive-table'
+  for (let i = 0; i < tables.length; i++) {
+    const table = tables[i];
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('responsive-table');
+    table.parentNode.insertBefore(wrapper, table);
+    wrapper.appendChild(table);
+  }
+}
+
+makeTablesResponsive()
--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,8 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from docutils.nodes import Element, raw
+import re
 
 project = "Liberatii Gateway"
 copyright = "2023 Liberatii. All rights reserved"
@@ -25,23 +27,24 @@ html_title = "Liberatii Gateway"
 
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-   "logo": {
-      # "image_light": "_static/light-theme-logo.svg",
-      # "image_dark": "_static/dark-theme-logo.svg",
-      "alt_text": "",
-      "text": "Liberatii Docs"
-   },
-   "navbar_center": ["search-field"],
-   "navbar_end": ["company-nav"],
-   "navbar_persistent": [],
-   "navigation_depth": 3,
-   "navigation_with_keys": False,
-   "search_bar_text": "Search docs...",
-   "footer_start": ["version", "copyright"],
-   "footer_end": ["footer-end"],
+    "logo": {
+        # "image_light": "_static/light-theme-logo.svg",
+        # "image_dark": "_static/dark-theme-logo.svg",
+        "alt_text": "",
+        "text": "Liberatii Docs"
+    },
+    "navbar_center": ["search-field"],
+    "navbar_end": ["company-nav"],
+    "navbar_persistent": [],
+    "navigation_depth": 3,
+    "navigation_with_keys": False,
+    "search_bar_text": "Search docs...",
+    "footer_start": ["version", "copyright"],
+    "footer_end": ["footer-end"],
+    "show_prev_next": False,
 }
 html_context = {
-   "default_mode": "light"
+    "default_mode": "light"
 }
 html_sidebars = {
     "**": ["sidebar-nav-bs"]
@@ -52,11 +55,20 @@ html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
 html_show_sourcelink = False
 
-# RST included at the beginning of every source file
-rst_prolog = """
-.. role:: popover-html(raw)
-   :format: html
-
-"""
-
 pygments_style = "xcode"
+
+
+def process_popover_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    """Processes the :popover: role to create raw HTML for the popover effect."""
+    split_text = re.split(r'(?<=\S)\s*:\s*(?=\S)', text)
+    title, content = split_text
+    html = f'<span class="popover-html"><button type="button" class="popover-role" data-bs-toggle="popover" ' \
+           f'data-bs-custom-class="custom-popover" data-bs-placement="top" data-bs-trigger="focus" ' \
+           f'data-bs-title="{title}" ' \
+           f'data-bs-content="{content}">{title}</button></span>'
+    return [raw('', html, format='html')], []
+
+
+def setup(app):
+    """Set up the Sphinx extension."""
+    app.add_role('popover', process_popover_role)
